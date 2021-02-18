aa9f4ef68bb3336e6af7af9ca179fb1596063a71
Jhonatan Mejia <quenandres@gmail.com>
2021-02-18 18:51:55 -0500

    Sockets funcionales

---
+++ sockets/socket.js
@@ -0,0 +1,13 @@
+// Mensajes de sockets
+const { io } = require('../index');
+
+io.on('connection', client => {
+    console.log('Cliente conectado');
+    client.on('disconnect', () => { 
+        console.log('Cliente desconectado');
+     });
+     client.on('mensaje', ( payload ) => {
+        console.log('mensajirijillo', payload);
+        io.emit('mensaje', { admin: 'Nuevo mensaje' });
+     });
+});
